6488d27472033f96e70d056c1654c4c6bdcce9cf
ben9523 <1025533206@qq.com>
2025-06-28 21:58:22 +0800

    Add files via upload

---
+++ src/main/java/org/example/demo02/Controller/TestController.java
@@ -0,0 +1,13 @@
+package org.example.demo02.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/home/test")
+    public String Test(){
+        return "Test";
+    }
+}
---
+++ src/main/java/org/example/demo02/Demo02Application.java
@@ -0,0 +1,14 @@
+package org.example.demo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Demo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo02Application.class, args);
+    }
+
+}
